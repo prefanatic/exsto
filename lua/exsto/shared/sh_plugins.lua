5476981696c355b35a42b9305ab0bf64f9a7d2f5
Cody Goldberg <dumbplanet424@gmail.com>
2013-06-26 17:18:48 -0400

    Fixed plugin bug

--- lua/exsto/shared/sh_plugins.lua
+++ lua/exsto/shared/sh_plugins.lua
@@ -91,7 +91,7 @@ function exsto.HookCall( name, gm, ... )
 			elseif data[1] == false then -- It returned an error, catch it.
 				exsto.ErrorNoHalt( "Hook '" .. name .. "' failed in plugin '" .. plug:GetID() .. "' error: " )
 				exsto.ErrorNoHalt( data[2] )
-				exsto.Plugins[ _ ]:Disable( 1 )
+				if SERVER then exsto.Plugins[ _ ]:Disable( 1 ) end
 			end
 		end
 	end
