b4ad8b4bf084558fb81a199555b07c376a0b7aaa
Cody Goldberg <dumbplanet424@gmail.com>
2013-07-07 12:40:31 -0400

    Added #153

--- lua/exsto/plugins/server/color-command.lua
+++ lua/exsto/plugins/server/color-command.lua
@@ -10,8 +10,33 @@ PLUGIN:SetInfo({
 	Owner = "Prefanatic",
 } )
 
-function PLUGIN:Color( self, ply, r, g, b )
+function PLUGIN:UnColor( caller, ply )
+	if not ply._OldColor then
+		caller:Print( exsto_CHAT, COLOR.NAME, ply:Nick(), COLOR.NORM, " has not been set a color before." )
+		return
+	end
+	
+	ply:SetPlayerColor( ply._OldColor )
+	ply._OldColor = nil
+	return { COLOR.NAME, caller:Nick(), COLOR.NORM, " has restored ", COLOR.NAME, ply:Nick(), COLOR.NORM, "'s color." }
+end
+PLUGIN:AddCommand( "uncolor", {
+	Call = PLUGIN.UnColor,
+	Desc = "Allows users to remove previously set colors on players.",
+	Console = { "uncolor" },
+	Chat = { "!uncolor" },
+	Arguments = {
+		{ Name = "Victim", Type = COMMAND_PLAYER };
+	};
+	Category = "Fun"
+})
+PLUGIN:RequestQuickmenuSlot( "uncolor", "Uncolor" )
 
+function PLUGIN:Color( self, ply, r, g, b )
+	
+	if not ply._OldColor then -- We only want to do this once, so we grab the first color he "spawns" with.
+		ply._OldColor = ply:GetPlayerColor()
+	end
 	ply:SetPlayerColor( Vector(r, g, b) )
 	
 	return {
@@ -24,12 +49,15 @@ function PLUGIN:Color( self, ply, r, g, b )
 end
 PLUGIN:AddCommand( "color", {
 	Call = PLUGIN.Color,
-	Desc = "Allows users to color people and make pink babies.",
+	Desc = "Allows users to color players.",
 	Console = { "color" },
 	Chat = { "!color" },
-	ReturnOrder = "Victim-Red-Green-Blue",
-	Args = {Victim = "PLAYER", Red = "NUMBER", Green = "NUMBER", Blue = "NUMBER"},
-	Optional = {Red = 255, Green = 255, Blue = 255},
+	Arguments = {
+		{ Name = "Victim", Type = COMMAND_PLAYER };
+		{ Name = "Red", Type = COMMAND_NUMER, Optional = 255 };
+		{ Name = "Green", Type = COMMAND_NUMBER, Optional = 255 };
+		{ Name = "Blue", Type = COMMAND_NUMBER, Optional = 255 };
+	};
 	Category = "Fun"
 })
 PLUGIN:RequestQuickmenuSlot( "color", "Color", {
